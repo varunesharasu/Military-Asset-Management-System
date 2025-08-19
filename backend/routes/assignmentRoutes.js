3121eb99025a87a230774ca8b4986b880a6e79a3
VaruneshT <tvarunesharasu@gmail.com>
2025-08-19 20:00:05 +0530

    Updated assignmentRoutes.js

--- backend/routes/assignmentRoutes.js
+++ backend/routes/assignmentRoutes.js
@@ -1,4 +1,10 @@
-// Assignment Routes
+// backend/routes/assignmentRoutes.js
 const express = require('express');
+const { protect, authorize } = require('../middleware/auth');
+const { createAssignment, getAssignments } = require('../controllers/assignmentController');
 const router = express.Router();
+
+router.post('/', protect, authorize('Admin', 'Base Commander'), createAssignment);
+router.get('/', protect, getAssignments);
+
 module.exports = router;
