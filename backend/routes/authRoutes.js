a2b905574684aca5e23ec5cb75b728959e34ff26
VaruneshT <tvarunesharasu@gmail.com>
2025-08-19 19:58:16 +0530

    Updated authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,7 @@
-// Auth Routes
+// backend/routes/authRoutes.js
 const express = require('express');
+const { loginUser } = require('../controllers/authController');
 const router = express.Router();
+
+router.post('/login', loginUser);
 module.exports = router;
