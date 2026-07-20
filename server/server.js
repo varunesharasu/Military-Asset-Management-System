dd067bd66f3c4f9f5900b04f191030cd39b66e5c
Varunesh T <tvarunesharasu@gmail.com>
2026-07-20 17:46:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Routes
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/assets", require("./routes/assets"))
 app.use("/api/purchases", require("./routes/purchases"))
